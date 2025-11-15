cba4a0a9fa8f18f3ff6f63fac4fa104a8feb3602
niknak1379 <nikanostovan@gmail.com>
2025-11-15 16:52:17 -0700

    should fix build, but theme is messed up

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Geo",
+        header: "Expletus Sans",
         body: "Expletus Sans",
         code: "IBM Plex Mono",
       },
